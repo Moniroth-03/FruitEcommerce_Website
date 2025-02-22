a6245881ec489180aa5c7459f78fce51fceeaba8
Moniroth-03 <127386659+Moniroth-03@users.noreply.github.com>
2025-02-22 11:55:54 +0700

    Install framer motion React Library

---
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {motion, AnimatePresence} from "framer-motion";
+const ResponsiveMenu = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ResponsiveMenu
